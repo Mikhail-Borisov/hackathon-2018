2b0736aa2d7e7fb55829a29c141eb610624674ba
Djamal <Nyarly51@gmail.com>
2018-10-10 00:21:25 +0300

    Add app launcher

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import { resolve } from 'path';
 import { AppConfigBuilder } from './components/configBuilder/AppConfigBuilder';
+import { Application } from './components/server/Application';
 
 const configBuilder = new AppConfigBuilder(resolve(__dirname, '../config'));
 configBuilder.print();
+
+new Application([], [])
+    .setHost('localhost')
+    .setPort(3000)
+    .setWorkers(2)
+    .run();
---
+++ src/components/server/Application.ts
@@ -0,0 +1,74 @@
+import * as Express from 'express';
+import { createExpressServer } from 'routing-controllers';
+import * as cluster from 'cluster';
+
+type ServerConfig = {
+    host: string;
+    port: number;
+    workers: number;
+};
+
+class Application {
+    private app: Express.Application;
+    private config: ServerConfig;
+    public static DEFAULT_CONFIG: ServerConfig = { host: 'localhots', port: 3000, workers: 1 };
+
+    constructor(
+        controllers: Function[] | string[],
+        middlewares: Function[] | string[],
+        config: ServerConfig = Application.DEFAULT_CONFIG
+    ) {
+        this.config = config;
+        this.app = createExpressServer({
+            defaultErrorHandler: true,
+            controllers,
+            middlewares
+        });
+    }
+
+    public setPort(port: number): Application {
+        this.config.port = port;
+        return this;
+    }
+
+    public setHost(host: string): Application {
+        this.config.host = host;
+        return this;
+    }
+
+    public setWorkers(workerNum: number): Application {
+        this.config.workers = workerNum;
+        return this;
+    }
+
+
+    public run(): void {
+        if (this.config.workers === 1) {
+            this.launch();
+        } else {
+            this.clusterLaunch();
+        }
+    }
+
+    protected launch(): void {
+        const { host, port } = this.config;
+        this.app.listen({ host, port }, () => {
+            console.log(`Server started at http://${host}:${port}`);
+        });
+    }
+
+    protected clusterLaunch(): void {
+        if (cluster.isMaster) {
+            const workersCount = this.config.workers;
+            console.log(`Starting ${workersCount} workers`);
+            for (let i = 0; i < workersCount; i++) {
+                cluster.fork();
+            }
+        } else {
+            this.launch();
+        }
+    }
+
+}
+
+export { Application, ServerConfig };
