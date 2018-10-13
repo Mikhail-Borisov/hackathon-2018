a138dd89464c54022636a063db1f128a05ccb72e
Mikhail Borisov <miborisov@nes.ru>
2018-10-13 19:00:53 +0300

    Fix CORS

--- src/components/server/middlewares/CorsMiddleware.ts
+++ src/components/server/middlewares/CorsMiddleware.ts
@@ -1,7 +1,15 @@
 import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
-import * as cors from 'cors';
+import { NextFunction } from 'express';
 
 @Middleware({ type: 'before' })
 export class CorsMiddleware implements ExpressMiddlewareInterface {
-    public use = cors();
+    public use(_request: any, response: any, next: NextFunction) {
+        response.set({
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Credentials': 'true'
+        });
+        next();
+    }
 }
